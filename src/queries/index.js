b76e85ebfd94b03282528aef0e805addae4b53a5
“a_villarejo19” <a.villarejo19@gmail.com>
2019-02-04 01:55:09 -0600

    Añadiendo consultas de modelo Productos

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,29 +1,52 @@
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 export const CLIENTES_QUERY = gql`
-	query getClientes($limite: Int, $offset: Int) {
-		clientes: getClientes(limite: $limite, offset: $offset) {
-			id
-			nombre
-			apellido
-			empresa
-		}
-		total: totalClientes
-	}
+  query getClientes($limite: Int, $offset: Int) {
+    clientes: getClientes(limite: $limite, offset: $offset) {
+      id
+      nombre
+      apellido
+      empresa
+    }
+    total: totalClientes
+  }
 `;
 
 export const CLIENTE_QUERY = gql`
-	query getCliente($id: ID) {
-		cliente: getCliente(id: $id) {
-			id
-			nombre
-			apellido
-			empresa
-			emails {
-				email
-			}
-			edad
-			tipo
-		}
-	}
+  query getCliente($id: ID) {
+    cliente: getCliente(id: $id) {
+      id
+      nombre
+      apellido
+      empresa
+      emails {
+        email
+      }
+      edad
+      tipo
+    }
+  }
+`;
+
+export const PRODUCTOS_QUERY = gql`
+  query getProductos($limite: Int, $offset: Int) {
+    productos: getProductos(limite: $limite, offset: $offset) {
+      id
+      nombre
+      stock
+      precio
+    }
+    total: totalProductos
+  }
+`;
+
+export const PRODUCTO_QUERY = gql`
+  query getProducto($id: ID) {
+    producto: getProductos(id: $id) {
+      id
+      nombre
+      stock
+      precio
+    }
+  }
 `;
